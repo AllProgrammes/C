ed4ab346f0bf28cdb6e7b325c503291b24980e72
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-02-12 19:26:08 +0530

    Write a programme to print hello world

---
+++ HELLO-WORLD.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h> //header file//
+int main()         // could have been written void main but idk why my vscode gives error when writing void //
+{
+    printf("HELLO WORLD"); //';' is called terminator (it is used to end the line or say end of line )
+    return 0;              // it removes the garbage value left after compilation//
+}
