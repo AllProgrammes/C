781e4395caf42a5541476fe246cad676bd52f1b7
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-03-19 00:22:26 +0530

    Fixed variable 'number' showing wrong value in the output screen

--- Chapter 7 - Array/To Practice/03_table_by_user.c
+++ Chapter 7 - Array/To Practice/03_table_by_user.c
@@ -5,13 +5,13 @@ int main()
     int value[10];
     printf("Enter the number : ");
     scanf("%d", &number);
-    for (int i = 1; i <= 10; i++)
+    for (int i = 0; i < 10; i++)
     {
-        value[i] = number * i;
+        value[i] = number * (i + 1);
     }
-    for (int i = 1; i <= 10; i++)
+    for (int i = 0; i < 10; i++)
     {
-        printf("%d x %d = %d\n", number, i, value[i]);
+        printf("%d x %d = %d\n", number, (i + 1), value[i]);
     }
     return 0;
 }
--- Chapter 7 - Array/To Practice/03_table_predefined.c
+++ Chapter 7 - Array/To Practice/03_table_predefined.c
@@ -3,13 +3,13 @@ int main()
 {
     int number = 5;
     int value[10];
-    for (int i = 1; i <= 10; i++)
+    for (int i = 0; i < 10; i++)
     {
-        value[i] = number * i;
+        value[i] = number * (i + 1);
     }
-    for (int i = 1; i <= 10; i++)
+    for (int i = 0; i < 10; i++)
     {
-        printf("%d x %d = %d\n", number, i, value[i]);
+        printf("%d x %d = %d\n", number, (i + 1), value[i]);
     }
     return 0;
 }
