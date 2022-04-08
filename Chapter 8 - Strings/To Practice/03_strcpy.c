e686958ce6d8b2afcae0566a32f4c01cc229e657
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-04-09 00:11:53 +0530

    Modified program to perform it's work correctly

--- Chapter 8 - Strings/To Practice/03_strcpy.c
+++ Chapter 8 - Strings/To Practice/03_strcpy.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
-int strcpy(char target[], char source[]);
-int strcpy(char target[], char source[])
+#include <string.h>
+void strcopy(char name[], char *ptr)
 {
     int i = 0;
-    while (source[i] != '\0')
+    while (name[i] != '\0')
     {
-        target[i] = source[i];
+        /*At the first loop when i = 0 :-
+        pointer ptr was equal to target[0]
+        and further with every loop when we did p++ ,
+        in reality it was happening target[1],target[2] and so on */
+        *ptr = name[i]; // target[0]=name[0]
+        ptr++;          // target[0] was made target[1] here
         i++;
     }
-
-    printf("After using the funtion strcpy the value of the string 'copy' is %s", source);
-    return 0;
+    *ptr = '\0';
 }
 int main()
 {
-    char copy[30];
-    char name[] = "Biswajit";
-    strcpy(copy, name);
+    char name[] = "biswajit"; // made an array -->name
+    char target[10];          // made a blank array of 10 characters -->target
+    char *ptr = target;       // made a pointer to store the value of target[0];
+    strcopy(name, ptr);       // passed the whole -name- array and value of --ptr(address of target)--;
+    printf("After using the funtion strcpy the value of the string 'target' is %s\n", target);
     return 0;
 }
