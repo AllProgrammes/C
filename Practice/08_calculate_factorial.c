dbb39a6c4aff1632db5b410a0f688da1eb630c51
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-04-10 12:58:31 +0530

    Write a program to calculate factorial value of the given number

---
+++ Practice/08_calculate_factorial.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n, sum = 1;
+    printf("Enter the value of n : ");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        sum = sum * i;
+    }
+    printf("Factorial of %d is %d", n, sum);
+    return 0;
+}
